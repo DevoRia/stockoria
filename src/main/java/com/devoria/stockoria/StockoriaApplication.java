ae0cd0b60cf784354eba0a0dfd6e249c65966ecb
DevoRia <the.rocky.vadim@gmail.com>
2022-01-12 23:15:12 +0200

    init spring

---
+++ src/main/java/com/devoria/stockoria/StockoriaApplication.java
@@ -0,0 +1,15 @@
+package com.devoria.stockoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class StockoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockoriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devoria/stockoria/StockoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoria.stockoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
