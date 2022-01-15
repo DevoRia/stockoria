85985493b3395d2a43d9696779267535f36c0c56
DevoRia <the.rocky.vadim@gmail.com>
2022-01-15 22:10:59 +0200

    add aws cognito jwt authorization

--- src/main/java/com/devoria/stockoria/controllers/UserController.java
+++ src/main/java/com/devoria/stockoria/controllers/UserController.java
@@ -1,13 +1,28 @@
 package com.devoria.stockoria.controllers;
 
+import com.devoria.stockoria.models.Login;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/user")
-    public String getInfo() {
+    @GetMapping("/health")
+    public HttpStatus health() {
+        return OK;
+    }
+
+
+    @GetMapping("/api/user")
+    public String getInfo(HttpServletRequest req) {
         return "HelloU";
     }
 
---
+++ src/main/java/com/devoria/stockoria/models/Login.java
@@ -0,0 +1,17 @@
+package com.devoria.stockoria.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Login {
+
+    private String username;
+    private String password;
+
+}
